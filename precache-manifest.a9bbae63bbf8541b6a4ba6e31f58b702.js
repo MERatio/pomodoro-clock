791267a509cff2a327f9da3f27f637dbac5e297e
Mark Edward Ratio <markedwardratio@gmail.com>
2020-01-17 10:42:13 +0800

    Updates

---
+++ precache-manifest.a9bbae63bbf8541b6a4ba6e31f58b702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18988885456fd5de8f0aff99a6db6e8c",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "5be8ded499c414d810fe",
+    "url": "/pomodoro-clock/static/css/main.8aad2312.chunk.css"
+  },
+  {
+    "revision": "c5c6791276e61084b3ce",
+    "url": "/pomodoro-clock/static/js/2.a42a0897.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pomodoro-clock/static/js/2.a42a0897.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5be8ded499c414d810fe",
+    "url": "/pomodoro-clock/static/js/main.28846bfd.chunk.js"
+  },
+  {
+    "revision": "44fb5c8ce35f8fc282d8",
+    "url": "/pomodoro-clock/static/js/runtime-main.039fd173.js"
+  }
+]);
